50f41d86ab5be023c1ddb32b2cfb3de713c842a4
Ike Isenhour <isenhouralbert@gmail.com>
2024-09-19 23:56:14 -0400

    chore: updating project structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"os"
 
-	"github.com/IkeIsenhour/leetcode-go/internal/problems"
+	"github.com/IkeIsenhour/leetcode-go/internal/structure/linkedlist"
+	"github.com/IkeIsenhour/leetcode-go/internal/structure/listnode"
 )
 
 func main() {
-	ans := problems.AngleClock(12, 0)
-	fmt.Println(ans)
+	c := listnode.NewListNode(3, nil)
+	b := listnode.NewListNode(2, c)
+	a := listnode.NewListNode(1, b)
+	ll := linkedlist.NewLinkedList(a)
+
+	ll.Print(os.Stdout)
 }
