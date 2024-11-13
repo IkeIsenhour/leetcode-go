9d52b0402a2899029c420646c3287a9440ec8f82
Ike Isenhour <isenhouralbert@gmail.com>
2024-11-12 22:15:09 -0500

    chore: add slice equality assertion helper

--- internal/utility/test_helpers.go
+++ internal/utility/test_helpers.go
@@ -1,11 +1,22 @@
 package utility
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func AssertEquality[T comparable](t testing.TB, got, want T) {
 	t.Helper()
 
 	if got != want {
+		t.Errorf("got: %v, want %v", got, want)
+	}
+}
+
+func AssertSliceEquality[E comparable, T ~[]E](t testing.TB, got, want T) {
+	t.Helper()
+
+	if !slices.Equal(got, want) {
 		t.Errorf("got: %v, want %v", got, want)
 	}
 }
