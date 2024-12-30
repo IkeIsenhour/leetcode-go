f40fd3003144b3e3cb2153f3af40fe44165eca50
Ike Isenhour <isenhouralbert@gmail.com>
2024-12-29 23:03:59 -0500

    problem: product of array except self

--- internal/problem/product_of_array_except_self.go
+++ internal/problem/product_of_array_except_self.go
@@ -1,5 +1,35 @@
 package problem
 
 func ProductOfArrayExceptSelf(nums []int) []int {
-	return []int{0}
+	product := 1
+	zeroFound := false
+
+	for _, v := range nums {
+		if v == 0 {
+			if zeroFound {
+				ans := make([]int, len(nums))
+				return ans
+			}
+			zeroFound = true
+		} else {
+			product *= v
+		}
+	}
+
+	ans := make([]int, len(nums))
+	if zeroFound {
+		for k, v := range nums {
+			if v == 0 {
+				ans[k] = product
+			} else {
+				ans[k] = 0
+			}
+		}
+	} else {
+		for k, v := range nums {
+			ans[k] = product / v
+		}
+	}
+
+	return ans
 }
