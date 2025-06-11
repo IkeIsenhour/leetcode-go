2ac42b9729b586d4701db823fdddb8adc8d78d31
Ike Isenhour <isenhouralbert@gmail.com>
2025-06-10 23:04:20 -0500

    docs: general rules for solving these problems

---
+++ docs/rulesOfThumb.md
@@ -0,0 +1,1 @@
+1. If we are given an array and asked to write an algorithm that runs in O(log n) time, it probably want binary search
--- internal/problem/ransom_note.go
+++ internal/problem/ransom_note.go
@@ -3,7 +3,9 @@ package problem
 // CanConstructOne uses 2 maps to identify if we have reached enough occurrances of a character.
 // It loops through both strings to crearte a map of each. tc: O(n + m)
 // We store both those loops in 2 sepearte maps. sc: O(n + m)
-// Then we loop though one of the maps again so the. tc: O(n + m + m)
+// Then we loop though one of the maps again so the.
+// tc: O(n + m + m)
+// sc: O(n + m)
 
 func CanConstructOne(ransomNote string, magazine string) bool {
 	rMap := make(map[rune]int)
@@ -29,6 +31,31 @@ func CanConstructOne(ransomNote string, magazine string) bool {
 		if mMap[k] < v {
 			return false
 		}
+	}
+
+	return true
+}
+
+// CanConstructTwo very similar to the above, but is able to remove one map.
+// tc: O(n + m)
+// sc: O(n)
+func CanConstructTwo(ransomNote string, magazine string) bool {
+
+	mMap := make(map[rune]int)
+	for _, v := range magazine {
+		if _, ok := mMap[v]; !ok {
+			mMap[v] = 1
+		} else {
+			mMap[v]++
+		}
+	}
+
+	for _, v := range ransomNote {
+		if v, ok := mMap[v]; !ok || v <= 0 {
+			return false
+		}
+
+		mMap[v]--
 	}
 
 	return true
